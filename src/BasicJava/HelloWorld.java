ecf5c55da61d355439d352e205705a5b3645ec3c
rahul <rahul@DESKTOP-EI1VT41>
2024-04-02 01:00:08 +0530

    Added new codes

--- src/BasicJava/HelloWorld.java
+++ src/BasicJava/HelloWorld.java
@@ -5,23 +5,154 @@ import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		Scanner s =new Scanner(System.in);
-		int n=s.nextInt();
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		int i=1;
 		while(i<=n) {
 			int j=1;
-			while(j<=n-i) {
-				System.out.print(" ");
-				j=j+1;
-			}
-			int k=1;
-			while(k<=2*i-1) {
+			while(j<=n) {
 				System.out.print("*");
-				k=k+1;
+				j++;
 			}
 			System.out.println();
-			i=i+1;
+			i++;
 		}
+		
+		
+		// Examples of inc and dec
+//		int a=5;
+//		int b=7;
+//		if(a++>=5 && b--<7) {
+//			System.out.print("inside if");
+//		}
+//		System.out.println(a);
+//		System.out.println(b);
+		
+		
+//		if(a++>5 || b--<7) {
+//			System.out.print("inside if");
+//		}
+//		System.out.println(a);
+//		System.out.println(b);
+		
+		
+		
+		
+		//Increment/Decrement operators
+//		int i=5;
+//		int a=++i;
+//		int b=(i--)+(++i);
+//		int c=(++a)+(b--)+(--i);
+//		System.out.println(i);
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+		
+		
+		
+		//Scope
+//		int n=5;
+//		int i=1;
+//		while(i<=n) {
+//			int j=2;
+//			System.out.print(j);
+//			j=j+1;
+//			i=i+1;
+//		}
+//		if(i==5) {
+//			int x=2;
+//		}
+//		System.out.print(x);
+		
+		
+		
+		
+		
+		
+		//Inverted Triangle
+//		Scanner s =new Scanner(System.in);
+//		int n=s.nextInt();
+//		int i=1;
+//		while(i<=n) {
+//			int j=1;
+//			while(j<=i-1) {
+//				System.out.print(" ");
+//				j=j+1;
+//			}
+//			int k=1;
+//			while(k<=n-i+1) {
+//				System.out.print("*");
+//				k=k+1;
+//			}
+//			System.out.println();
+//			i=i+1;
+//		}
+		//Full Diamond
+//		Scanner s =new Scanner(System.in);
+//		int n=s.nextInt();
+//		int i=1;
+//		while(i<=n) {
+//			int j=1;
+//			while(j<=n-i) {
+//				System.out.print(" ");
+//				j=j+1;
+//			}
+//			int k=1;
+//			while(k<=2*i-1) {
+//				System.out.print("*");
+//				k=k+1;
+//			}
+//			System.out.println();
+//			i=i+1;
+//		}
+//		i=1;
+//		while(i<=n-1) {
+//			int j=1;
+//			while(j<=i) {
+//				System.out.print(" ");
+//				j=j+1;
+//			}
+//			int k=1;
+//			while(k<=2*(n-i)-1) {
+//				System.out.print("*");
+//				k=k+1;
+//			}
+//			System.out.println();
+//			i=i+1;
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		Half Diamond
+//		Scanner s =new Scanner(System.in);
+//		int n=s.nextInt();
+//		int i=1;
+//		while(i<=n) {
+//			int j=1;
+//			while(j<=n-i) {
+//				System.out.print(" ");
+//				j=j+1;
+//			}
+//			int k=1;
+//			while(k<=2*i-1) {
+//				System.out.print("*");
+//				k=k+1;
+//			}
+//			System.out.println();
+//			i=i+1;
+//		}
 		
 		
 		
